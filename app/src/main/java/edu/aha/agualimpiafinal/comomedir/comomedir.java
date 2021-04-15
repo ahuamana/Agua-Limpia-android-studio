bc88c93ac107fc4b71cd40824ad559931702223c
Alikhan <73962071@continental.edu.pe>
2021-04-15 17:29:37 -0500

    Se cambio el diseño de como medir y se agrego un dialogo en cada tipo de test

--- app/src/main/java/edu/aha/agualimpiafinal/comomedir/comomedir.java
+++ app/src/main/java/edu/aha/agualimpiafinal/comomedir/comomedir.java
@@ -2,18 +2,30 @@ package edu.aha.agualimpiafinal.comomedir;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import org.jetbrains.annotations.NotNull;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import edu.aha.agualimpiafinal.R;
@@ -23,6 +35,9 @@ public class comomedir extends Fragment {
     private ComomedirViewModel mViewModel;
 
     ImageView imgHealthMetric,imgWaterSafe;
+    Button btnHealthMetric, btnWaterSafe;
+    Dialog dialog;
+    String videoId = "";
 
     public static comomedir newInstance() {
         return new comomedir();
@@ -37,6 +52,36 @@ public class comomedir extends Fragment {
         imgHealthMetric = vista.findViewById(R.id.CMivHealtMetric);
         imgWaterSafe = vista.findViewById(R.id.CMivWaterSafe);
 
+        //Inicializar el dialogo con el contexto
+        Context context= vista.getContext();
+        dialog=new Dialog(context);
+        //
+
+        btnHealthMetric= vista.findViewById(R.id.CMbtnHealthMetric_masinfo);
+        btnWaterSafe=vista.findViewById(R.id.CMbtnWaterSafe_masinfo);
+
+
+        //Inicio al hacer click el buttom WaterSafe
+        btnWaterSafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoId="rT-l8VnHx_Q";
+                //Log.e("HIzoclick",videoId);
+                OpenDialog(videoId,"Water Safe");
+            }
+        });
+        //
+
+        //Inicio al hacer click el buttom HeatlMetric
+        btnHealthMetric.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoId="_4VTtEJMjHc";
+                //Log.e("HIzoclick",videoId);
+                OpenDialog(videoId,"Health Metric");
+            }
+        });
+        //
 
 
         Glide.with(this)
@@ -51,6 +96,31 @@ public class comomedir extends Fragment {
 
         return vista;
     }
+
+    private void OpenDialog(final String videoID, String titulo) {
+        Log.e("DialogoconelID",videoID);
+        dialog.setContentView(R.layout.custom_video_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        //Declarar variables del dialogo que se utilizan
+        TextView DItittle = dialog.findViewById(R.id.DItittle);
+        DItittle.setText(titulo);
+        YouTubePlayerView video = dialog.findViewById(R.id.DIvideoYoutube);
+
+        getLifecycle().addObserver(video);
+
+        video.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NotNull YouTubePlayer youTubePlayer) {
+
+                youTubePlayer.loadVideo(videoID,0);
+            }
+        });
+
+        dialog.show();
+    }
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
