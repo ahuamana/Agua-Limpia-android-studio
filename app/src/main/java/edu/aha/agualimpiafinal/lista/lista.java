ec82f17a8f773c7cb2f39633eb654e39702d6d35
Alikhan <73962071@continental.edu.pe>
2021-06-03 13:22:13 -0500

    added new way to search (provincia) on lista

--- app/src/main/java/edu/aha/agualimpiafinal/lista/lista.java
+++ app/src/main/java/edu/aha/agualimpiafinal/lista/lista.java
@@ -42,7 +42,7 @@ public class lista extends Fragment  implements SearchView.OnQueryTextListener {
     //Referencias para Cloudfirestore
     FirebaseFirestore fStore;
     FirebaseAuth fAuth;
-    private SearchView svSearch;
+    private SearchView svSearch, svSearchProvincia;
 
 
     private ListaViewModel mViewModel;
@@ -64,6 +64,38 @@ public class lista extends Fragment  implements SearchView.OnQueryTextListener {
         //inicializar variables para buscar
         svSearch= (SearchView) vista.findViewById(R.id.Isearch);
         initListener();
+
+        //Implementado segunda forma de buscar
+        svSearchProvincia = vista.findViewById(R.id.IsearchProvincia);
+        svSearchProvincia.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //codigo implementar
+                Log.e("data cambiado", "cambiando a provincia");
+
+                adapter=null;
+                Log.e("mensajebusqueda: ",newText.toLowerCase());
+                FirestoreRecyclerOptions <MoldeMuestra> newoptions = new FirestoreRecyclerOptions.Builder<MoldeMuestra>()
+                        .setQuery(fStore.collection("DatosMuestra").orderBy("MuestraProvincia").startAt(newText.toLowerCase()).limit(25).endAt(newText.toLowerCase()+'\uf8ff'),MoldeMuestra.class)
+                        .build();
+
+                //enviar los datos al adapter
+                adapter=new MuestrasAdapter(newoptions);
+                adapter.startListening();
+
+                //asignar datos al recyclerView
+                recyclerUsuarios.setAdapter(adapter);
+
+
+                ////
+                return false;
+            }
+        });
 
         //codigo
         //iniciar arraylist y refrencia al contenedor
@@ -117,6 +149,8 @@ public class lista extends Fragment  implements SearchView.OnQueryTextListener {
     private  void initListener(){
         svSearch.setOnQueryTextListener(this);
     }
+
+
 
     @Override
     public boolean onQueryTextSubmit(String query) {
