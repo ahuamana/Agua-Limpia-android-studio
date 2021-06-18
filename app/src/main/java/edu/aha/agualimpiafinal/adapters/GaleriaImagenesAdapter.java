9043e1a547d829f1abb0ed336f1ad8cbb224f647
Alikhan <73962071@continental.edu.pe>
2021-06-17 19:49:24 -0500

    set scaletype on Glide to center image

--- app/src/main/java/edu/aha/agualimpiafinal/adapters/GaleriaImagenesAdapter.java
+++ app/src/main/java/edu/aha/agualimpiafinal/adapters/GaleriaImagenesAdapter.java
@@ -24,10 +24,17 @@ public class GaleriaImagenesAdapter extends BaseAdapter {
     public GaleriaImagenesAdapter(Context mContext) {
 
         this.mContext = mContext;
-        imagenesArray.add("https://www.peru-retail.com/wp-content/uploads/Real-Plaza-nueva-imagen-1024x411.jpg");
+        imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1706202118%3A47%3A18.jpg?alt=media&token=1706202118:47:18.jpg");
         imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1606202122%3A51%3A28.jpg?alt=media&token=1606202122:51:28.jpg");
         imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1606202122%3A51%3A23.jpg?alt=media&token=1606202122:51:23.jpg");
         imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1606202122%3A51%3A12.jpg?alt=media&token=1606202122:51:12.jpg");
+        imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1706202118%3A59%3A33.jpg?alt=media&token=1706202118:59:33.jpg");
+        imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1606202122%3A51%3A12.jpg?alt=media&token=1606202122:51:12.jpg");
+        imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1606202122%3A51%3A28.jpg?alt=media&token=1606202122:51:28.jpg");
+        imagenesArray.add("https://firebasestorage.googleapis.com/v0/b/agualimpia-image.appspot.com/o/1706202118%3A47%3A18.jpg?alt=media&token=1706202118:47:18.jpg");
+        imagenesArray.add("https://www.lavanguardia.com/files/article_main_microformat/files/fp/uploads/2020/08/06/5f2bd32938d7b.r_d.593-707-0.jpeg");
+        imagenesArray.add("https://www.avina.net/wp-content/uploads/2020/06/webinar-acceso-al-agua-fundacion-avina.jpg");
+        imagenesArray.add("https://www.comexperu.org.pe/upload/images/economia-231017-104654.jpg");
 
     }
 
@@ -59,7 +66,8 @@ public class GaleriaImagenesAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         //Codigo para mostrar imagenes
         ImageView imageView = new ImageView(mContext);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setAdjustViewBounds(true);
 //        imageView.setImageResource(imagenesArray[position]);
 //        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
 //        imageView.setLayoutParams( new ViewGroup.LayoutParams(
@@ -70,7 +78,8 @@ public class GaleriaImagenesAdapter extends BaseAdapter {
 //        ///
         Glide.with(mContext)
                 .load(imagenesArray.get(position))
-                .apply(new RequestOptions().override(600,600))  //set new size from each image
+                .centerCrop()
+                .apply(new RequestOptions().override(340,350))  //set new size from each image
                 .placeholder(R.drawable.photography_background)
                 .into(imageView);
 
