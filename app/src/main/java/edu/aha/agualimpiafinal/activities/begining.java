4932a1f5100751dd6e4e58ad2205069d7ab56374
Alikhan <73962071@continental.edu.pe>
2021-07-13 13:20:20 -0500

    validate email when you add author data

--- app/src/main/java/edu/aha/agualimpiafinal/activities/begining.java
+++ app/src/main/java/edu/aha/agualimpiafinal/activities/begining.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -80,7 +82,7 @@ public class begining extends AppCompatActivity {
             if(valid_lastname)
             {
 
-                if(valid_email)
+                if(isValidEmail(BEedtEmail.getText().toString()))
                 {
                     //Guardar las preferencias
                     guardarPreferencias();
@@ -88,6 +90,9 @@ public class begining extends AppCompatActivity {
                     i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK); //eliminar activities anteriores
                     startActivity(i);
 
+                }else
+                {
+                    BEedtEmail.setError("Ingrese un correo válido!");
                 }
             }
 
@@ -144,6 +149,10 @@ public class begining extends AppCompatActivity {
 
         editor.commit();
 
+    }
+
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 
 
