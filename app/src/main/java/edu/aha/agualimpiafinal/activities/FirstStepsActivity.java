07afc4d7e9ade973d363f7afaeb226ca88d9657f
Alikhan <73962071@continental.edu.pe>
2021-07-14 12:26:07 -0500

    set funtions to go diferents activities

--- app/src/main/java/edu/aha/agualimpiafinal/activities/FirstStepsActivity.java
+++ app/src/main/java/edu/aha/agualimpiafinal/activities/FirstStepsActivity.java
@@ -2,12 +2,19 @@ package edu.aha.agualimpiafinal.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import edu.aha.agualimpiafinal.R;
 
 public class FirstStepsActivity extends AppCompatActivity {
+
+    TextView txtSaltarFirstSteps;
+    Button btnStartFirstSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,40 @@ public class FirstStepsActivity extends AppCompatActivity {
 
         setStatusBarColor();
 
+        txtSaltarFirstSteps = findViewById(R.id.txtsaltarFirstSteps);
+        btnStartFirstSteps = findViewById(R.id.btnIniciarFirstSteps);
 
+        btnStartFirstSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                goToInicio();
+
+            }
+        });
+
+        txtSaltarFirstSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                goToLogin();
+
+            }
+        });
+
+
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(FirstStepsActivity.this, begining.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void goToInicio()
+    {
+        Intent intent = new Intent(FirstStepsActivity.this, inicioApp.class);
+        startActivity(intent);
     }
 
     private void setStatusBarColor()
