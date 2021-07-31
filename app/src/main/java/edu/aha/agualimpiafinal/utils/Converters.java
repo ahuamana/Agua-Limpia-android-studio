63d17ddeb841f7bb29346185880669a2a7b1a793
Alikhan <73962071@continental.edu.pe>
2021-07-31 09:03:44 -0500

    change variables from static singleton method

--- app/src/main/java/edu/aha/agualimpiafinal/utils/Converters.java
+++ app/src/main/java/edu/aha/agualimpiafinal/utils/Converters.java
@@ -2,6 +2,7 @@ package edu.aha.agualimpiafinal.utils;
 
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +14,19 @@ public class Converters extends AppCompatActivity {
 
     public static Converters instance = new Converters();
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        instance = new Converters();
+    }
 
     public String epochTimeToDate(Long DateToConvert)
     {
+        long dateEpoch = DateToConvert*1000;// its need to be in milisecond
+        Date dateFormat = new java.util.Date(dateEpoch);
+        String dateFinal = new SimpleDateFormat("MM dd, yyyy hh:mma").format(dateFormat);
 
-        Date dateFormat = new java.util.Date(DateToConvert);
-        String dateConverted = new SimpleDateFormat("MM dd, yyyy hh:mma").format(dateFormat);
-
-        return dateConverted;
+        return dateFinal;
     }
 }
