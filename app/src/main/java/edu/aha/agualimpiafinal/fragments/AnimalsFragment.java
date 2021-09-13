950a8c97bf07cc46c30e918aabd12a774f162c2d
Alikhan <73962071@continental.edu.pe>
2021-09-13 09:39:43 -0500

    added animals fragment

---
+++ app/src/main/java/edu/aha/agualimpiafinal/fragments/AnimalsFragment.java
@@ -0,0 +1,52 @@
+package edu.aha.agualimpiafinal.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.aha.agualimpiafinal.R;
+import edu.aha.agualimpiafinal.databinding.FragmentAnimalsBinding;
+
+
+public class AnimalsFragment extends Fragment {
+
+    private FragmentAnimalsBinding binding;
+
+    public AnimalsFragment() {
+
+    }
+
+
+    public static AnimalsFragment newInstance(String param1, String param2) {
+        AnimalsFragment fragment = new AnimalsFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentAnimalsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+
+
+
+
+        return view;
+    }
+}
