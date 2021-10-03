0ef009c7becb9f32306734991b57c59e551c0f89
Alikhan <73962071@continental.edu.pe>
2021-10-03 01:09:20 -0500

    create bottom sheet dialogfragment with design

---
+++ app/src/main/java/edu/aha/agualimpiafinal/fragments/BottomSheetComentar.java
@@ -0,0 +1,47 @@
+package edu.aha.agualimpiafinal.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import edu.aha.agualimpiafinal.R;
+
+
+public class BottomSheetComentar extends BottomSheetDialogFragment {
+
+    public BottomSheetComentar() {
+
+    }
+
+
+    public static BottomSheetComentar newInstance(String param1, String param2) {
+        BottomSheetComentar fragment = new BottomSheetComentar();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.bottom_sheet_comentar, container, false);
+    }
+}
